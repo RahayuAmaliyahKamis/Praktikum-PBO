4990f57e9b828185a2e8c06c448cd9ab064af13e
RahayuAmaliyahKamis <160306918+RahayuAmaliyahKamis@users.noreply.github.com>
2024-03-15 08:59:06 +0700

    Percobaan 2

---
+++ Manajer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Percobaan2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Manajer extends Pegawai {
+    public String departemen;
+    
+    public void isiData(String n, String d) {
+        setNama(n);
+        departemen = d;
+    }
+}
