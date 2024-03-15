9cef4218313252293bbcf3bce63ce1a129ecf8cf
RahayuAmaliyahKamis <160306918+RahayuAmaliyahKamis@users.noreply.github.com>
2024-03-15 09:07:06 +0700

    Percobaan 3

---
+++ Child2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Percobaan3;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Child2 extends Parent2 {
+    int x;
+    
+    // Konstruktor kelas Child2
+    public Child2() {
+        x = 5;
+    }
+}
